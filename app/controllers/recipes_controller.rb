d7574324d87e1c930cb8dba4323b94161fdbe41f
santiago <santiagodelafuente2013@gmail.com>
2023-07-16 17:23:11 +0200

    Add Redis operations, and CRUD actions to recipes_controller
    This commit introduces redis operations within the recipes controller's index action. CRUD operations for a recipe were also added - Users can now edit, update, destroy the recipes. Furthermore, Redis gem's version was relaxed and imported into the application. Styling updates were made in recipes/index.html.erb, using Font Awesome Icons. Flash messages were also introduced, which appear after create, update and destroy actions.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,12 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:edit, :update, :destroy]
+
   def index
     @recipes = Recipe.all
+    $redis.set("mi_clave", "santiagoxD")
+
+    valor = $redis.get("mi_clave")
+    puts valor
   end
 
   def new
@@ -16,10 +22,34 @@ class RecipesController < ApplicationController
       render :new
     end
   end
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: 'Receta actualizada exitivamente.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+
+    if @recipe.destroy
+      flash[:notice] = 'Receta eliminada exitosamente.'
+    else
+      flash[:alert] = 'Hubo un error al eliminar la receta.'
+    end
+
+    redirect_to root_path
+  end
 
   private
 
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
   def recipe_params
     params.require(:recipe).permit(:name, :description)
+    # Asegúrate de actualizar la lista de parámetros permitidos con todos los atributos de tu modelo Recipe
   end
 end
