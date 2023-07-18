030267933fdd8a95c986bb25427afcb7759966da
santiago <santiagodelafuente2013@gmail.com>
2023-07-18 14:36:25 +0200

    "Implement flash message animation"
    Added an animation effect to the flash message for better user experience. Flash messages now slide from hidden to centered position on screen, remain there for a while and then slide out, instead of just fading out as before. This was done by modifying transform styles in the flash controller and adjusting CSS classes in the application layout view file.

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,11 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="flash"
 export default class extends Controller {
   connect() {
+    this.element.style.transform = "translate(-50%, 0)";
     this.fadeOut()
   }
 
   fadeOut() {
     setTimeout(() => {
+      this.element.style.transform = "translate(-50%, 100vh)";
       this.element.style.opacity = 0
     }, 2000)
   }
